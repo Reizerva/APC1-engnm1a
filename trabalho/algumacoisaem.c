5614a7bf72ce67e6ac568ac59d09cc5619b730f3
Reizerva <160796063+Reizerva@users.noreply.github.com>
2024-05-30 23:18:41 +0000

    trabalho

--- trabalho/algumacoisaem.c
+++ trabalho/algumacoisaem.c
@@ -7,24 +7,39 @@ int main() {
 //  printf("Suas peças: O/@\n Peças do seu oponente: X/#");
   float tabuleiro[8][8];
   for (int i = 0; i < 8; ++i) {
-    for (int j = 0; j < 8; ++j){
-      int l = 0;
-      tabuleiro[i][j] = l;
-      l++;
+    for (int j = 0; j < 8; ++j) {
+      tabuleiro[i][j] = i;
     }
   }
-/*  for (int i = 0; i < 8; ++i) {
+  //printf("\x1b[41m  \x1b[0m\n");
+  int deu_certo = system("clear");
+  for (int i = 0; i < 8; ++i) {
     for (int j = 0; j < 8; ++j) {
-       if ((int)tabuleiro[i][j] % 2 == 0) {
-          tabuleiro[i][j] = 0;
-         } else {tabuleiro[i][j] = 1;}
-        }
-    }*/
+       if (j % 2 == 0) {
+        if ((int)tabuleiro[i][j] % 2 == 0) {
+          //tabuleiro[i][j] = 1;
+          printf("\x1b[41m  \x1b[0m");
+         } else {
+          //tabuleiro[i][j] = 0;
+          printf("\x1b[103m  \x1b[0m");
+         }
+       } else {
+         if ((int)tabuleiro[i][j] % 2 == 0) {
+           //tabuleiro[i][j] = 0;
+           printf("\x1b[103m  \x1b[0m");
+         } else {
+           //tabuleiro[i][j] = 1;
+           printf("\x1b[41m  \x1b[0m");
+         }
+       }
+    }
+    printf("\n");
+  }/*
   for (int i = 0; i < 8; ++i) {
      for (int j = 0; j < 8; ++j){
-       printf("%d", (int)tabuleiro[i][j]);
+       printf("%d ", (int)tabuleiro[i][j]);
      }
     printf("\n");
-  }
+  }*/
   return 0;
 }
--- trabalho/test.c
+++ trabalho/test.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    int matrix[8][8];
-
-    // Initialize the matrix with some values (you can modify this part)
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            matrix[i][j] = i * 8 + j; // You can replace this with your own values
+  char tabuleiro[8][8];
+  int i, j;
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      if ((i + j) % 2 == 0) {
+        tabuleiro[i][j] = ' ';
+      } else {
+        if (i < 3) {
+          tabuleiro[i][j] = 'O';
+        } else if (i > 4) {
+          tabuleiro[i][j] = 'X';
+        } else {
+          tabuleiro[i][j] = ' ';
         }
+      }
     }
-
-    // Set even values to 0 and odd values to 1
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            if (matrix[i][j] % 2 == 0) {
-                matrix[i][j] = 0;
-            } else {
-                matrix[i][j] = 1;
-            }
-        }
+  }
+  int deu_certo = system("clear");
+  for (i = 0; i < 8; i++) {
+    for (j = 0; j < 8; j++) {
+      printf("%c ", tabuleiro[i][j]);
     }
-
-    // Print the modified matrix
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            printf("%d ", matrix[i][j]);
-        }
-        printf("\n");
+    printf("\n");
+  }
+  int vez = 1; // 0 é Jogador 0, 1 é Jogador X
+  while (vez == 1) {
+    printf("X, escolha a peça que deseja mover (linha coluna): ");
+    deu_certo = scanf("%d %d", &i, &j);
+    while (getchar() != '\n')
+      ;
+    if (tabuleiro[i][j] != 'X') {
+      printf("Nenhuma peça X encontrada.\n");
+    } else {
+      printf("X, escolha a posição para onde deseja mover a peça (linha "
+             "coluna): ");
+      deu_certo = scanf("%d %d", &i, &j);
     }
-
-    return 0;
+  }
+  return 0;
 }
